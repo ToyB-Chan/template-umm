6f2b55da59fe8bceab67a647f59f10b82e02a549
ToyB-Chan <jasjason0otto@gmail.com>
2024-11-30 13:11:18 +0100

    Exclude debugging symbols from packaging

--- package.ps1
+++ package.ps1
@@ -19,7 +19,7 @@ if ($NoArchive) {
 $ZipOutDir = "$ZipWorkDir/$modId"
 
 New-Item "$ZipOutDir" -ItemType Directory -Force
-Copy-Item -Force -Path $FilesToInclude -Destination "$ZipOutDir"
+Copy-Item -Force -Path $FilesToInclude -Destination "$ZipOutDir" -Exclude "*.pdb"
 
 if (!$NoArchive)
 {
